b6291d5717e5e334d9dfd04b324ab7fbe1ff1a14
CesarOcam <cesar_po21@hotmail.es>
2025-05-27 17:58:44 -0600

    Se subieron ejercicios en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+/*import React, { useState } from 'react';
 import VistaUsuario from './views/UsuarioForm';
 
 function App() {
@@ -29,6 +29,54 @@ function App() {
         onChange={handleChange}
         onSubmit={handleSubmit}
       />
+    </div>
+  );
+}
+
+export default App;*/
+
+import React from "react";
+
+const nombre = "Juan";
+const usuarioLogueado = true;
+const frutas = ["Manzana", "Banana", "Cereza"];
+
+function Saludo() {
+  return <h2>Hola desde un componente</h2>;
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Hola, React!</h1>
+      <p>Este es mi primer componente.</p>
+
+      <div>
+        <h1>Hola, {nombre}!</h1>
+        <p>2 + 3 = {2 + 3}</p>
+      </div>
+
+      <div>
+        {usuarioLogueado ? (
+          <p>Bienvenido!</p>
+        ) : (
+          <p>Por favor, inicia sesión.</p>
+        )}
+      </div>
+
+      <div>
+        <ul>
+          {frutas.map((fruta, index) => (
+            <li key={index}>{fruta}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <Saludo />{nombre}
+        <Saludo />
+      </div>
+      
     </div>
   );
 }
